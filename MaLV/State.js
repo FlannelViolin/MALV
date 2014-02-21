b47c064d81648210b3f1f7f56c29578c339b9beb
pbuttler <pdb7705@g.rit.edu>
2014-02-20 22:22:53 -0500

    Skeleton State script

---
+++ MaLV/State.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+// -------------- VARS ----------------
+//
+// radius
+// position
+// id
+//
+// list or hash of transitions
+//
+// Color
+// ------------------------------------
+
+// ------------ FUNCTIONS -------------
+//
+function display(){
+	// Draw the state with a circle
+	// Use radius and Color
+	
+	// Draw all attached transitions
+}
+
+function snapTransition(){
+	// MATH
+}
+
+function toggleSelect(){
+	// Change selection status
+}
+
+function addTransition( transition ){
+	// add a transition to the attached transition list
+}
+
