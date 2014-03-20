79f8d6cfce8d347f4b50db0c3fc1a0c5d5b69255
unknown <gap8240@SP2530-12.main.ad.rit.edu>
2014-03-20 16:53:59 -0400

    Will stop freezing if the machine is not valid

--- MaLV/inputCanvasBox.js
+++ MaLV/inputCanvasBox.js
@@ -28,17 +28,19 @@ function displayInputs(input,go){
 
 function drawReadingCharacters(index){
 	ictx.clearRect(0,0,inputCanvas.width,inputCanvas.height);
+	displayInputs(input,false);
 	var currentString = checkedInputs[checkedInputs.length - 1];
 	for(var i = 0; i < index; i++){
 		ictx.fillText("Checking input: ",10, 20 +checkedInputs.length * bufferX);
 		ictx.fillText(currentString[i], 100+(7*i),20 +checkedInputs.length*bufferX);
-		console.log("should be writing chras" + i);
+	
 	}
 	
 }
 
 function setAcceptedForInput (inputAccepted){
 	didAccept.push(inputAccepted);
+	animating = false;
 }
 
 function clearInputCanvas(){
