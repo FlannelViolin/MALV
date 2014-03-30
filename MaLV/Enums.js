a3d88755a3c223a5c9b6e8b918e2edb07c7ce42f
unknown <gap8240@SP2530-12.main.ad.rit.edu>
2014-03-30 18:49:29 -0400

    added a new enum for input accept states, added a test file to eventually replace InputCanvasBox (adding a 'dict' and using my new enum)

--- MaLV/Enums.js
+++ MaLV/Enums.js
@@ -12,3 +12,9 @@ var PlacementMode = {
 	NOTHING:			{value: 4}
 	
 };
+
+var AcceptedForInput = {
+	ACCEPTED:			{value: 0},
+	NOTACCEPTED:		{value: 1},
+	IMPOSSIBLE:			{value: 2} // no transition found, etc
+}
---
+++ MaLV/testNewInputCanvasBox.js
@@ -0,0 +1,64 @@
+var bufferX = 15;
+
+checkedInputs []; // array of inputs[]s 
+input = []; // 2 index arry of all a single input and its relative accepted state 
+
+// takes a new input 
+function takeInputs(newInput,go){
+	ictx.font="10px Georgia";
+	
+	for(i in checkedInputs){
+		ictx.fillStyle = 'black';
+		ictx.fillText("Checked Input: " + checkedInputs[i][0],10, 10 +i*bufferX);
+	}
+	
+	
+	if(go){
+		
+		// set up the 'dict'
+		input.push(newInput);
+		input.push(null);
+		checkedInputs.push(input);
+		go = false;
+	}
+	drawAccepted();
+
+}
+
+function drawAccepted(){
+	line(inputCanvas.width-60,0,inputCanvas.width-60,inputCanvas.height,ictx);
+	for(i in checkedInputs){
+		if(checkedInputs[i][1] == ACCEPTED){
+			ictx.fillStyle = '#AAFF00';
+			ictx.fillText("O",inputCanvas.width-50,10+i*bufferX);
+		}
+		if(checkedInputs[i][1]== NOTACCEPTED){
+			ictx.fillStyle = '#AA0000';
+			ictx.fillText("X",inputCanvas.width-50,10+i*bufferX);
+		}
+		if(checkedInputs[i][1]== IMPOSSIBLE){
+			ictx.fillStyle = '#FFFFFF';
+			ictx.fillText("-",inputCanvas.width-50,10+i*bufferX);
+		}
+		
+	}
+}
+
+function setAcceptedForInput (inputAccepted){
+		for(i in checkedInputs){
+			if(checkedInputs[i][1] ==null({ // find the latest null value, fill it in
+				checkedInputs[i][1] = inputAccpted;
+				break;
+			}
+		}
+}
+
+function clearInputCanvas(){
+	checkedInputs = [];
+}
+
+function clearAccepted(){
+	for(i in checkedInputs){
+		checkedInputs[i][1] = null;
+	}
+}
